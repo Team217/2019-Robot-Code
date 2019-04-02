b41ba0b12979d48e7073d8af31b553eb9e675122
Noah <Noah.Vermeulen1@gmail.com>
2019-04-02 18:23:39 -0400

    Created the AutonTelescope command and an AutonDelay command

---
+++ src/main/java/frc/robot/commands/auton/AutonDelay.java
@@ -0,0 +1,45 @@
+package frc.robot.commands.auton;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.*;
+
+/**
+ * Delays an action in auton control mode.
+ * 
+ * @author ThunderChickens 217
+ */
+public class AutonDelay extends Command {
+
+    /**
+     * Delays an action in auton control mode.
+     * 
+     * @param timeout
+     *        The delay, in seconds
+     * 
+     * @author ThunderChickens 217
+     */
+    public AutonDelay(double timeout) {
+        setTimeout(timeout);
+    }
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    @Override
+    protected void end() {
+    }
+
+    @Override
+    protected void interrupted() {
+    }
+}
--- src/main/java/frc/robot/commands/auton/AutonTelescope.java
+++ src/main/java/frc/robot/commands/auton/AutonTelescope.java
@@ -1,32 +1,69 @@
 package frc.robot.commands.auton;
 
+import org.team217.pid.*;
+
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.*;
 
+/**
+ * Runs the telescope in auton control mode using {@code APID}.
+ * 
+ * @author ThunderChickens 217
+ */
 public class AutonTelescope extends Command {
+    APID apid = RobotMap.telescopeAPID;
+    double tar = 0;
 
-    public AutonTelescope() {
+    /**
+     * Runs the telescope in auton control mode using {@code APID}.
+     * 
+     * @param target
+     *        The {@code PID} target
+     * 
+     * @author ThunderChickens 217
+     */
+    public AutonTelescope(double target) {
         requires(Robot.kTelescopeSubsystem);
+        tar = target;
+    }
+
+    /**
+     * Runs the telescope in auton control mode using {@code APID}.
+     * 
+     * @param target
+     *        The {@code PID} target
+     * @param timeout
+     *        The time before automatically ending the command, in seconds
+     * 
+     * @author ThunderChickens 217
+     */
+    public AutonTelescope(double target, double timeout) {
+        this(target);
+        setTimeout(timeout);
     }
 
     @Override
     protected void initialize() {
+        apid.initialize();
     }
 
     @Override
     protected void execute() {
+        Robot.kTelescopeSubsystem.set(apid.getOutput(RobotMap.telescope.getEncoder(), tar));
     }
 
     @Override
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     @Override
     protected void end() {
+        Robot.kTelescopeSubsystem.set(0);
     }
 
     @Override
     protected void interrupted() {
+        Robot.kTelescopeSubsystem.set(0);  
     }
 }
